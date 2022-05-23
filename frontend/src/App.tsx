bc53b4c50a452bc742b2e6c721c21372dadfaaa3
DiegoVP66 <dvpdev33@gmail.com>
2022-05-23 17:12:58 -0300

    Frontend: utils/types/ AuthContextData

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 
 import RoutesComponent from "routes";
+import { useState } from "react";
+import { AuthContext, AuthContextData } from "AuthContextData";
 
 function App() {
+  const [authContextData, setAuthContextData] = useState<AuthContextData>({
+    authenticated: false,
+  });
   return (
-    <div className="App">
+    <AuthContext.Provider value={{ authContextData, setAuthContextData }}>
       <RoutesComponent />
-    </div>
+    </AuthContext.Provider>
   );
 }
 
